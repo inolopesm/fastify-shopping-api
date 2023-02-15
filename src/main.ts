cf19bc398a8840bb2003eb830fbe8512b69d340f
Matheus Inocencio <matheus.inocencio@novaescola.org.br>
2023-02-15 15:01:07 -0300

    feat: create GET /accounts + GET /products routes

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,20 @@ const PORT = env.get("PORT").default("3000").asPortNumber();
 
 const fastify = Fastify({ logger: true });
 
+fastify.get("/accounts", async function () {
+  return [
+    { id: 1, username: "user_one" },
+    { id: 2, username: "user_two" },
+    { id: 3, username: "user_three" },
+  ];
+});
+
+fastify.get("/products", async function () {
+  return [
+    { id: 1, name: "product_one" },
+    { id: 2, name: "product_two" },
+    { id: 3, name: "product_three" },
+  ];
+});
+
 fastify.listen({ port: PORT });
